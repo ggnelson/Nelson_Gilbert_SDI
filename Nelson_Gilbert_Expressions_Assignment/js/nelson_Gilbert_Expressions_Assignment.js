abf391b8574bbaf68026a9987b3ccd7184c05f71
ggnelson <gilbertnelson@Gilberts-MBP.home>
2015-09-10 21:10:13 -0400

    setting up equation

--- Nelson_Gilbert_Expressions_Assignment/js/nelson_Gilbert_Expressions_Assignment.js
+++ Nelson_Gilbert_Expressions_Assignment/js/nelson_Gilbert_Expressions_Assignment.js
@@ -15,3 +15,22 @@ var data = prompt("Please enter your service carrier.");
 
 // use the variable to tell user what plan he has
 alert("Your plan with "+data+" uses 10 GB a month. ");
+
+//see how many phones are in the plan
+
+var numberPhones = prompt(" How many phones are on your plan?");
+
+//console.log the answer for the equation
+console.log(numberPhones);
+
+//how many GB did the users phone use this month
+var phoneData = prompt(" How many GB did your phone use this month");
+
+//console.log the answer for the equation
+console.log(phoneData);
+
+//What amount equals 100%
+var monthlyGB = 10;
+
+//tell them how much data your phone uses
+
