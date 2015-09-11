4e9373b9335c6eb1face118448120af5763cc2e2
ggnelson <gilbertnelson@Gilberts-MBP.home>
2015-09-10 20:50:32 -0400

    created first alert

--- Nelson_Gilbert_Expressions_Assignment/js/nelson_Gilbert_Expressions_Assignment.js
+++ Nelson_Gilbert_Expressions_Assignment/js/nelson_Gilbert_Expressions_Assignment.js
@@ -5,4 +5,13 @@ Expressions
 9-10-2015
  */
 
-alert("Test to see if connected.");
+//alert("Test to see if connected.");
+
+//Create a calculator for which phone is using the most data for the month
+
+//ask the user for the amount of data they have a month
+
+var data = prompt("Please enter your service carrier.");
+
+// use the variable to tell user what plan he has
+alert("Your plan with "+data+" uses 10 GB a month. ");
